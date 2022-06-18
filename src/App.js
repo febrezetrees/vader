e94da5278531ed2e3cebbd50f984ea93a5d3a133
RWP <ryanpieszko@gmail.com>
2022-06-18 23:37:04 +1000

    updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coming soon
+          {`Rome wasn't built in a day :)`}
         </p>
       </header>
     </div>
